bc0d87627a665f077c56452f59989dcc06579ced
Shean-D <sheandivyan01@gmail.com>
2025-08-06 18:21:06 +0530

    Initialie the project

---
+++ src/main/java/main/Starter.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Login page");
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_form.fxml"))));
+        stage.show();
+    }
+}
